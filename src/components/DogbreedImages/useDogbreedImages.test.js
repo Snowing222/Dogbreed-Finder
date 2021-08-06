a33ba9e0ada073a346707de4c3546fe52464d809
snowing222 <snowing222@gmail.com>
2021-08-06 05:41:07 -0400

    useDogImages.test.js

---
+++ src/components/DogbreedImages/useDogbreedImages.test.js
@@ -0,0 +1,28 @@
+import useDogbreedImages from "./useDogbreedImages";
+import "whatwg-fetch";
+import { renderHook } from "@testing-library/react-hooks";
+import fetchMock from "fetch-mock";
+import { act } from "react-test-renderer";
+
+
+describe("useDogbreedImages should render list of images for a specific dog breeds ", () => {
+  beforeAll(() => {
+    global.fetch = fetch;
+  });
+  afterAll(() => {
+    fetchMock.restore();
+  });
+  
+
+  it("should return data with a successful request", async () => {
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'), // use actual for all non-hook parts
+  useParams: () => ({
+    breed: "hound"
+  }),
+  useRouteMatch: () => ({ url: 'https://dog.ceo/api/breed/hound/images' }),
+}));
+
+  });
+});
